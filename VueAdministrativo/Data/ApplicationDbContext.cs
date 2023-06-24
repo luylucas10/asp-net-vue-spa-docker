1c71a844bf820f3874edc0715e0297c42ed4348f
Luy Lucas <luylucas10@gmail.com>
2023-06-24 14:50:05 -0300

    Inicial

---
+++ VueAdministrativo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using VueAdministrativo.Models;
+
+namespace VueAdministrativo.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+	public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+		: base(options, operationalStoreOptions)
+	{
+	}
+}
