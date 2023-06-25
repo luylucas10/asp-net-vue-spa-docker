84f8b5305eaa58c43aef03f3f024e6272434e237
Luy Lucas <luylucas10@gmail.com>
2023-06-25 18:59:11 -0300

    API
    Configuração para registrar controllers;
    Remoção de projetos exemplos;

---
+++ src/AspNetDockerVue/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetDockerVue.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		public IActionResult Index() =>
+			Ok(new { message = "Mensagem da controller" });
+	}
+}
--- src/AspNetDockerVue/Program.cs
+++ src/AspNetDockerVue/Program.cs
@@ -5,10 +5,21 @@ namespace AspNetDockerVue
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+			builder.Services.AddControllers();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
 			var app = builder.Build();
 
-			app.MapGet("/", () => "Hello World!");
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
 
+			app.UseHttpsRedirection();
+			app.UseAuthorization();
+			app.MapControllers();
 			app.Run();
 		}
 	}
